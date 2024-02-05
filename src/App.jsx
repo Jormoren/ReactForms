fa8311a959062703603eb6080f67649d3704bf18
jorgitom1980 <141953007+jorgitom1980@users.noreply.github.com>
2024-02-04 20:11:45 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+import "./App.css";
+
+export default function App() {
+  const [token, setToken] = useState(null);
+  return (
+    <div className="app">
+      <SignUpForm onSetToken={setToken} className="sign-up-form" />
+      <Authenticate token={token} />
+    </div>
+  );
+}
